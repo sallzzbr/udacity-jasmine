e09236d84125e8575ad98d0408775bf116ea2147
sallzzbr <evio.salgado@hotmail.com>
2018-10-08 17:36:49 -0300

    done

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -14,6 +14,7 @@ $(function() {
     * feeds definitions, the allFeeds variable in our application.
     */
     describe('RSS Feeds', function() {
+
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -26,35 +27,58 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+         it('URL defined && not empty', function() {
+              allFeeds.forEach(function(feed) {
+                  expect(feed.url).toBeDefined();
+                  expect(feed.url.length).not.toBe(0);
+              });
+          });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('Name defined && not empty', function() {
+            allFeeds.forEach(function(feed) {
+                  expect(feed.name).toBeDefined();
+                  expect(feed.name.length).not.toBe(0);
+              });
+          });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The Menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('hidden by default', function() {
+              expect($(document.body).hasClass('menu-hidden')).toBe(true);
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it('togglable', function() {
+              $('.menu-icon-link').click();
+                expect($(document.body).hasClass('menu-hidden')).toBe(false);
+
+              $('.menu-icon-link').click();
+                expect($(document.body).hasClass('menu-hidden')).toBe(true);
+          });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -62,11 +86,39 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done) {
+              loadFeed(0, done);
+         });
+         it('There is at least a single .entry element in the .feed container', function() {
+              expect( $('.feed .entry').length).toBeGreaterThan(0);
+         });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        var feed;
+        var loadedFeed;
+
+        beforeEach(function(done) {
+           loadFeed(0,function() {
+               feed = $('.feed .entry > h2').html();
+               done();
+           });
+        });
+
+        it('content changes when loadFeed() is updated', function(done) {
+           loadFeed(1, function() {
+               loadedFeed = $('.feed .entry > h2').html();
+               expect(loadedFeed).not.toBe(feed);
+               done();
+           });
+        });
+
+    });
+
 }());
